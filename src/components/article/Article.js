431cad3775dd9239d3491238398d9aa33b2cb695
dllo <dllo@yongchuangtianya.local>
2017-08-25 20:25:41 +0800

    张霆

---
+++ src/components/article/Article.js
@@ -0,0 +1,24 @@
+/**
+ * Created by dllo on 17/8/25.
+ */
+import React, {Component} from 'react'
+import '../../assets/styles/article/article.styl'
+import Register from './Register'
+class Article extends Component {
+  render () {
+    return (
+      <div>
+        <div className='out'>
+          <div className='inside clear'>
+            <span className='left color'>原创文章</span>
+            <a href='' className='right font-size display'>发布</a>
+            <a href='' className='right display-a font-size'>预览</a>
+            <a href='' className='right display-a font-size'>保存</a>
+          </div>
+          <Register />
+        </div>
+      </div>
+    )
+  }
+}
+export default Article
---
+++ src/modules/article/App.js
@@ -0,0 +1,15 @@
+/**
+ * Created by dllo on 17/8/25.
+ */
+import React, {Component} from 'react'
+import Article from '../../components/article/Article'
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <Article />
+      </div>
+    )
+  }
+}
+export default App
