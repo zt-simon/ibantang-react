1b40e0278c264404a66062e0ca512c9063036340
dllo <dllo@123456.local>
2017-08-30 08:46:23 +0800

    777

--- src/modules/firstpage-about/App.js
+++ src/modules/firstpage-about/App.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react'
+import '../../assets/styles/host/about.styl'
+import About from '../../components/firstpage-about/firstpage-about'
 class App extends Component {
   render () {
     return (
       <div className='wrap'>
-        a
+        <About />
       </div>
     )
   }
