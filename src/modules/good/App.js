412903d3a9f10cd82930463a3c38d833bf203619
岳增臣 <yuezengchen@Yzc.local>
2017-09-01 16:03:14 +0800

    yzc

--- src/modules/good/App.js
+++ src/modules/good/App.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react'
 import Header from '../../components/top/Header'
-import TopControl from '../../components/top/TopControl'
+// import Search from '../../components/top/Search'
 import Main from '../../components/good/Main'
 class App extends Component {
   render () {
     return (
       <div>
         <Header />
-        <TopControl />
         <Main />
       </div>
     )
