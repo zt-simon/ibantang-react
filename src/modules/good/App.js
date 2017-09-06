71825ba3943824dda03381fe96cc1daf5ef1e811
岳增臣 <yuezengchen@Yzc.local>
2017-09-06 10:20:20 +0800

    yzc

--- src/modules/good/App.js
+++ src/modules/good/App.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react'
 import Header from '../../components/top/Header'
-// import Search from '../../components/top/Search'
+import Scroll from '../../components/index/scrollTop'
 import Main from '../../components/good/Main'
 import Footer from '../../components/index/Footer'
+import '../../assets/styles/good/scroll.styl'
 class App extends Component {
   render () {
     return (
       <div>
         <Header />
         <Main />
+        <div className="scroll">
+          <Scroll />
+        </div>
         <Footer />
       </div>
     )
